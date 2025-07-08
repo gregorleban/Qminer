02662a0b4a008cd25d5e0a1f8201e0611bd169ae
Gregor Leban <gleban@gmail.com>
2025-07-08 10:05:15 +0200

    - added build directory for uvlib

--- src/third_party/libuv-build.bat
+++ src/third_party/libuv-build.bat
@@ -1,2 +1,2 @@
-"c:\Program Files\Microsoft Visual Studio\2022\Enterprise\Common7\IDE\devenv.com" /build "Debug|x64" libuv\build\libuv.sln
+REM "c:\Program Files\Microsoft Visual Studio\2022\Enterprise\Common7\IDE\devenv.com" /build "Debug|x64" libuv\build\libuv.sln
 "c:\Program Files\Microsoft Visual Studio\2022\Enterprise\Common7\IDE\devenv.com" /build "Release|x64" libuv\build\libuv.sln
---
+++ src/third_party/libuv/build/CTestTestfile.cmake
@@ -0,0 +1,36 @@
+# CMake generated Testfile for 
+# Source directory: E:/Temp/11/libuv
+# Build directory: E:/Temp/11/libuv/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+if(CTEST_CONFIGURATION_TYPE MATCHES "^([Dd][Ee][Bb][Uu][Gg])$")
+  add_test(uv_test "E:/Temp/11/libuv/build/Debug/uv_run_tests.exe")
+  set_tests_properties(uv_test PROPERTIES  WORKING_DIRECTORY "E:/Temp/11/libuv" _BACKTRACE_TRIPLES "E:/Temp/11/libuv/CMakeLists.txt;715;add_test;E:/Temp/11/libuv/CMakeLists.txt;0;")
+elseif(CTEST_CONFIGURATION_TYPE MATCHES "^([Rr][Ee][Ll][Ee][Aa][Ss][Ee])$")
+  add_test(uv_test "E:/Temp/11/libuv/build/Release/uv_run_tests.exe")
+  set_tests_properties(uv_test PROPERTIES  WORKING_DIRECTORY "E:/Temp/11/libuv" _BACKTRACE_TRIPLES "E:/Temp/11/libuv/CMakeLists.txt;715;add_test;E:/Temp/11/libuv/CMakeLists.txt;0;")
+elseif(CTEST_CONFIGURATION_TYPE MATCHES "^([Mm][Ii][Nn][Ss][Ii][Zz][Ee][Rr][Ee][Ll])$")
+  add_test(uv_test "E:/Temp/11/libuv/build/MinSizeRel/uv_run_tests.exe")
+  set_tests_properties(uv_test PROPERTIES  WORKING_DIRECTORY "E:/Temp/11/libuv" _BACKTRACE_TRIPLES "E:/Temp/11/libuv/CMakeLists.txt;715;add_test;E:/Temp/11/libuv/CMakeLists.txt;0;")
+elseif(CTEST_CONFIGURATION_TYPE MATCHES "^([Rr][Ee][Ll][Ww][Ii][Tt][Hh][Dd][Ee][Bb][Ii][Nn][Ff][Oo])$")
+  add_test(uv_test "E:/Temp/11/libuv/build/RelWithDebInfo/uv_run_tests.exe")
+  set_tests_properties(uv_test PROPERTIES  WORKING_DIRECTORY "E:/Temp/11/libuv" _BACKTRACE_TRIPLES "E:/Temp/11/libuv/CMakeLists.txt;715;add_test;E:/Temp/11/libuv/CMakeLists.txt;0;")
+else()
+  add_test(uv_test NOT_AVAILABLE)
+endif()
+if(CTEST_CONFIGURATION_TYPE MATCHES "^([Dd][Ee][Bb][Uu][Gg])$")
+  add_test(uv_test_a "E:/Temp/11/libuv/build/Debug/uv_run_tests_a.exe")
+  set_tests_properties(uv_test_a PROPERTIES  WORKING_DIRECTORY "E:/Temp/11/libuv" _BACKTRACE_TRIPLES "E:/Temp/11/libuv/CMakeLists.txt;736;add_test;E:/Temp/11/libuv/CMakeLists.txt;0;")
+elseif(CTEST_CONFIGURATION_TYPE MATCHES "^([Rr][Ee][Ll][Ee][Aa][Ss][Ee])$")
+  add_test(uv_test_a "E:/Temp/11/libuv/build/Release/uv_run_tests_a.exe")
+  set_tests_properties(uv_test_a PROPERTIES  WORKING_DIRECTORY "E:/Temp/11/libuv" _BACKTRACE_TRIPLES "E:/Temp/11/libuv/CMakeLists.txt;736;add_test;E:/Temp/11/libuv/CMakeLists.txt;0;")
+elseif(CTEST_CONFIGURATION_TYPE MATCHES "^([Mm][Ii][Nn][Ss][Ii][Zz][Ee][Rr][Ee][Ll])$")
+  add_test(uv_test_a "E:/Temp/11/libuv/build/MinSizeRel/uv_run_tests_a.exe")
+  set_tests_properties(uv_test_a PROPERTIES  WORKING_DIRECTORY "E:/Temp/11/libuv" _BACKTRACE_TRIPLES "E:/Temp/11/libuv/CMakeLists.txt;736;add_test;E:/Temp/11/libuv/CMakeLists.txt;0;")
+elseif(CTEST_CONFIGURATION_TYPE MATCHES "^([Rr][Ee][Ll][Ww][Ii][Tt][Hh][Dd][Ee][Bb][Ii][Nn][Ff][Oo])$")
+  add_test(uv_test_a "E:/Temp/11/libuv/build/RelWithDebInfo/uv_run_tests_a.exe")
+  set_tests_properties(uv_test_a PROPERTIES  WORKING_DIRECTORY "E:/Temp/11/libuv" _BACKTRACE_TRIPLES "E:/Temp/11/libuv/CMakeLists.txt;736;add_test;E:/Temp/11/libuv/CMakeLists.txt;0;")
+else()
+  add_test(uv_test_a NOT_AVAILABLE)
+endif()
